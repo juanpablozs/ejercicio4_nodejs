fe38a84a91deacfa1043682e6c03f277272aa2c3
juanpablo <juanpablo.zuritasoto@gmail.com>
2023-11-19 18:56:38 +0100

    Página creada y login funciona.

--- Proyecto/routes/index.js
+++ Proyecto/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  const elementos = ['Elemento 1', 'Elemento 2', 'Elemento 3'];
+  res.render('index', { title: 'Express', elementos });
+});
+
+router.get('/login', function(req, res, next) {
+	res.render(path.join(__dirname, '../views/login'), { title: 'Login' });
 });
 
 module.exports = router;
